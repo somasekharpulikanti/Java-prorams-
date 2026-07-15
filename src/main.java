4d2a15d637af7c6794430e71892637b454402f58
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-15 15:27:05 +0530

    Second Largest number

--- src/main.java
+++ src/main.java
@@ -5,12 +5,32 @@
 //    }
 //}
 //basic for loop program
+//class Main{
+//    public static void main(String[]args){
+//        int[] arr = {10,20,30,40,50,60};
+//        for(int i = 0;i<=5;i++){
+//            System.out.println(arr[i]);
+//
+//        }
+//    }
+//}
+// second largest numbrr
 class Main{
     public static void main(String[]args){
-        int[] arr = {10,20,30,40,50,60};
-        for(int i = 0;i<=5;i++){
-            System.out.println(arr[i]);
-
+        int[] arr = {10,20,30,40,50};
+        int largest = arr[0];
+        int seclargest = arr[1];
+        for(int i=0;i<=4;i++){
+            if(arr[i] > largest ){
+                largest = arr[i];
+            }
         }
+        for(int i= 0;i<=4;i++){
+            if(arr[i] < largest && arr[i] > seclargest){
+                seclargest = arr[i];
+            }
+        }
+        System.out.println("Largest-Number :" +largest);
+        System.out.println("Second-Largest Number :" +seclargest);
     }
 }
