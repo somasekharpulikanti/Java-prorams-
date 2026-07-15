f63a74705d4e604109eddbc044d045e916310fdd
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-15 16:00:46 +0530

    Prime Number Cheking

--- src/main.java
+++ src/main.java
@@ -14,7 +14,7 @@
 //        }
 //    }
 //}
-// second largest numbrr
+//SECOND LARGEST NUMBER
 //class Main{
 //    public static void main(String[]args){
 //        int[] arr = {10,20,30,40,50};
@@ -34,7 +34,7 @@
 //        System.out.println("Second-Largest Number :" +seclargest);
 //    }
 //}
-//Move Zeros to the end
+//MOVE ZEROS TO THE END
 //class Main{
 //    public static void main(String[]args){
 //        int[] arr = {10,0,2,0,4,5,0};
@@ -54,21 +54,50 @@
 //        }
 //    }
 //}
-//palindrome
+//PALINDROME
+//class Main{
+//    public static void main(String[]args){
+//        int a = 121;
+//        int temp  = a;
+//        int digit = 0;
+//        int rev = 0;
+//        while(temp > 0){
+//            digit = temp % 10;
+//            rev = rev * 10 + digit;
+//            temp = temp/10;
+//        }
+//        if(a == rev){
+//            System.out.println(rev);
+//
+//        }
+//    }
+//}
+//LARGEST NUMBER
+//class Main{
+//    public static void main(String[]args){
+//        int[] arr = {10,20,30,40,50};
+//        int largest = arr[0];
+//        for(int i =0;i<=4;i++){
+//            if(arr[i] > largest)
+//                largest = arr[i];
+//        }
+//        System.out.println("Largst Number :" +largest);
+//    }
+//}
+//PRIME NUMBER;
 class Main{
     public static void main(String[]args){
-        int a = 121;
-        int temp  = a;
-        int digit = 0;
-        int rev = 0;
-        while(temp > 0){
-            digit = temp % 10;
-            rev = rev * 10 + digit;
-            temp = temp/10;
+        int num = 7;
+        int count = 0;
+        for(int i = 1;i <= num;i++){
+            if(num % i == 0){
+                count ++;
+            }
         }
-        if(a == rev){
-            System.out.println(rev);
-
-        }
+        if(count == 2){
+                System.out.println("Prime Number");
+            }else{
+                System.out.println("NoN-Prime Number");
+            }
     }
 }
