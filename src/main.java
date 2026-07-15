ce489e6cd227038b23aeeebab4adb89dc21c7fd2
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-15 15:34:11 +0530

    Moves zeros to the End

--- src/main.java
+++ src/main.java
@@ -15,22 +15,42 @@
 //    }
 //}
 // second largest numbrr
+//class Main{
+//    public static void main(String[]args){
+//        int[] arr = {10,20,30,40,50};
+//        int largest = arr[0];
+//        int seclargest = arr[1];
+//        for(int i=0;i<=4;i++){
+//            if(arr[i] > largest ){
+//                largest = arr[i];
+//            }
+//        }
+//        for(int i= 0;i<=4;i++){
+//            if(arr[i] < largest && arr[i] > seclargest){
+//                seclargest = arr[i];
+//            }
+//        }
+//        System.out.println("Largest-Number :" +largest);
+//        System.out.println("Second-Largest Number :" +seclargest);
+//    }
+//}
+//Move Zeros to the end
 class Main{
     public static void main(String[]args){
-        int[] arr = {10,20,30,40,50};
-        int largest = arr[0];
-        int seclargest = arr[1];
-        for(int i=0;i<=4;i++){
-            if(arr[i] > largest ){
-                largest = arr[i];
+        int[] arr = {10,0,2,0,4,5,0};
+        int index = 0;
+        for(int i = 0;i<=6;i++){
+            if(arr[i] != 0){
+                arr[index] = arr[i];
+                index++;
             }
         }
-        for(int i= 0;i<=4;i++){
-            if(arr[i] < largest && arr[i] > seclargest){
-                seclargest = arr[i];
-            }
+        for(int i = index; i<=6;i++){
+            arr[i] = 0;
         }
-        System.out.println("Largest-Number :" +largest);
-        System.out.println("Second-Largest Number :" +seclargest);
+        for(int i =0;i<=6;i++){
+            System.out.println(arr[i]);
+
+        }
     }
 }
