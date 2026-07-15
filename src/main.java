4513acd4839a4736027ea135c5b2ba58dd6930cf
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-15 15:06:39 +0530

    Basic Loop program

--- src/main.java
+++ src/main.java
@@ -1,7 +1,16 @@
-public class main {
+//public class main {
+//    public static void main(String[]args){
+//        System.out.println("Hello World");
+//
+//    }
+//}
+//basic for loop program
+class Main{
     public static void main(String[]args){
-        System.out.println("Hello World");
+        int[] arr = {10,20,30,40,50,60};
+        for(int i = 0;i<=5;i++){
+            System.out.println(arr[i]);
 
+        }
     }
 }
-
