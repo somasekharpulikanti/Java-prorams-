7640965af5812aac036bc47c48581d9fd9439a03
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-20 10:03:39 +0530

    ROTATE THE ARRAY

--- src/main.java
+++ src/main.java
@@ -253,11 +253,15 @@
 class Main{
     public static void main(String[]args){
         int[] arr = {1,2,3,4,5};
+        // save the element
        int last  = arr.length;
+       //shifts the elemenst
        for(int i = arr.length-1;i>0;i--){
            arr[i] = arr[i-1];
        }
+       //assign teh last index to the first
        arr[0] =last;
+       //print the elemenst
        for(int i = 0; i<arr.length;i++){
            System.out.println(arr[i]);
        }
