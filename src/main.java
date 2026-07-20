d6d973b3a439ede1a06a7ee437b3a7e5a169069a
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-20 11:31:47 +0530

    ROTATE THE ARRAY TOWARDS THE LEFT

--- src/main.java
+++ src/main.java
@@ -249,21 +249,35 @@
 //        }
 //    }
 //}
-// ROTATE THE ARRAY
+// ROTATE THE ARRAY IN THE RIGHT POSITION
+//class Main{
+//    public static void main(String[]args){
+//        int[] arr = {1,2,3,4,5};
+//        // save the element
+//       int last  = arr.length;
+//       //shifts the elemenst
+//       for(int i = arr.length-1;i>0;i--){
+//           arr[i] = arr[i-1];
+//       }
+//       //assign teh last index to the first
+//       arr[0] =last;
+//       //print the elemenst
+//       for(int i = 0; i<arr.length;i++){
+//           System.out.println(arr[i]);
+//       }
+//    }
+//}
+//ROTATE THE ARRAY FROM IN THE LEFT POSITION
 class Main{
     public static void main(String[]args){
         int[] arr = {1,2,3,4,5};
-        // save the element
-       int last  = arr.length;
-       //shifts the elemenst
-       for(int i = arr.length-1;i>0;i--){
-           arr[i] = arr[i-1];
-       }
-       //assign teh last index to the first
-       arr[0] =last;
-       //print the elemenst
-       for(int i = 0; i<arr.length;i++){
-           System.out.println(arr[i]);
-       }
+        int frist = arr[0];
+        for(int i =0; i<4;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[4] = frist;
+        for(int i = 0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
