9f1b9993d348b20ced896e3dbf4840e006852539
Somasekhar <somasekharpulikanti0@gmail.com>
2026-07-27 20:09:18 +0530

    REVERSE RIGHT ANGLE TRAINGLE

--- src/main.java
+++ src/main.java
@@ -445,10 +445,31 @@
 //}
 // PATTERNS PATTERNS
 // RIGHT ANGLE TRIANGLE
+//class Main{
+//    public static void main(String[]args){
+//        for(int i =0;i<5;i++){
+//            for(int j = 0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+//    }
+//}
+//class Main{
+//    public static void main(String[]args){
+//        for(int i = 5;i>0;i--){
+//            for(int j = 5; j>=i;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+//    }
+//}
+// REVERSE RIGHT TRAINGLE
 class Main{
     public static void main(String[]args){
-        for(int i =0;i<5;i++){
-            for(int j = 0;j<=i;j++){
+        for(int i = 5;i>=1;i--){
+            for(int j = 0 ; j < i;j++){
                 System.out.print("*");
             }
             System.out.println(" ");
